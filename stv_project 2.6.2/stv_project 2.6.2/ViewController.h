eee1c6a4bd679698f94e5eb735c6f52940035445
kazua <kazua.kuro@gmail.com>
2018-02-19 20:45:26 +0900

    fixed. #2.6.wの課題が終了しました。

---
+++ stv_project 2.6.2/stv_project 2.6.2/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  stv_project 2.6.2
+//
+//  Created by kazua on 2018/02/15.
+//  Copyright © 2018年 kazua. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ViewController : UIViewController
+
+@property NSMutableArray *urlSchemeText;
+
+@end
+
