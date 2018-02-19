02d9b72ae3e7b1980c4b0cd823b8dfd80e37497d
kazua <kazua.kuro@gmail.com>
2018-02-19 19:48:04 +0900

    fixed. #2.1.2の課題が終了しました。

---
+++ stv_project 2.1.2/stv_project 2.1.2/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  stv_project 2.1.2
+//
+//  Created by kazua on 2018/01/19.
+//  Copyright © 2018年 kazua. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
