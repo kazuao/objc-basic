34bb9de8060bb60d43cb6474add7473f3cec04d1
kazua <kazua.kuro@gmail.com>
2018-02-19 20:33:04 +0900

    fixed. #2.5.4の課題が終了しました。

---
+++ stv_project 2.5.4/stv_project 2.5.4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  stv_project 2.5.4
+//
+//  Created by kazua on 2018/02/14.
+//  Copyright © 2018年 kazua. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ViewController : UIViewController
+
+
+@end
+
