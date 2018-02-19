ce9d25848fc8c55198f8f4a751d99be5bbbca22c
kazua <kazua.kuro@gmail.com>
2018-02-19 20:12:40 +0900

    fixed. #2.1.14の課題が終了しました。

---
+++ stv_project 2.1.14/stv_project 2.1.14/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  stv_project 2.1.14
+//
+//  Created by kazua on 2018/01/27.
+//  Copyright © 2018年 kazua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
