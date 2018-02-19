58a3636cb0ed2bd6c6a8c4a1faa2193fed3e23bb
kazua <kazua.kuro@gmail.com>
2018-02-19 19:44:23 +0900

    fixed. #1.1.3の課題が終了しました。

---
+++ stv_project 1.1.3/stv_project 1.13/ViewController.m
@@ -0,0 +1,83 @@
+//
+//  ViewController.m
+//  stv_project 1.13
+//
+//  Created by kazua on 2018/01/17.
+//  Copyright © 2018年 kazua. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // 課題
+    // if~else文
+    NSInteger ifcheckInt = 1;
+    if (ifcheckInt == 1){
+        NSLog(@"これは%ldです", ifcheckInt);
+    } else {
+        NSLog(@"これは1ではありません");
+    }
+    
+    // if~else if文
+    NSInteger ifCheckInt2 = 2;
+    if (ifCheckInt2 == 1){
+        NSLog(@"これは%ldです", ifCheckInt2);
+    }else if(ifCheckInt2 == 2){
+        NSLog(@"これは%ldです", ifCheckInt2);
+    }else{
+        NSLog(@"これは2ではありません");
+    }
+    
+    // 三項演算子
+    NSString *compareStr1 = @"a";
+    NSString *compareStr2 = @"b";
+    NSString *isSameStr = (compareStr1 == compareStr2)? @"同じ": @"違う";
+    NSLog(@"%@", isSameStr);
+    
+    // for文
+    for (int i = 0; i <= 10; i++){
+        NSLog(@"%d", i);
+    }
+    
+    // 高速列挙構文
+    NSArray *colorArr = @[@"赤", @"青", @"緑"];
+    for (NSString *color in colorArr){
+        NSLog(@"%@", color);
+    }
+    
+    NSDictionary *colorDic = @{@"red": @"赤", @"blue": @"青", @"green": @"緑"};
+    for (NSString *key in colorDic){
+        NSLog(@"%@: %@", key, colorDic[key]);
+    }
+    // switch文
+    NSInteger switchInt = 2;
+    switch (switchInt) {
+        case 1:
+            NSLog(@"これは%ldです", switchInt);
+            break;
+        case 2:
+            NSLog(@"これは%ldです", switchInt);
+            break;
+        case 3:
+            NSLog(@"これは%ldです", switchInt);
+            break;
+        default:
+            break;
+    }
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
