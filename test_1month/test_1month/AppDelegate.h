d73aa2ab01e38e6d0389158b46089ebb3ad33964
kazua <kazua.kuro@gmail.com>
2018-02-22 10:19:53 +0900

    first commit

---
+++ test_1month/test_1month/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test_1month
+//
+//  Created by kazua on 2018/02/22.
+//  Copyright © 2018年 kazua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test_1month/test_1monthTests/test_1monthTests.m
@@ -0,0 +1,39 @@
+//
+//  test_1monthTests.m
+//  test_1monthTests
+//
+//  Created by kazua on 2018/02/22.
+//  Copyright © 2018年 kazua. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface test_1monthTests : XCTestCase
+
+@end
+
+@implementation test_1monthTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
